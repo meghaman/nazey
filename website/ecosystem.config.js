fe3b2cdafbdc606fe9e57456c66433727c6a5f68
meghaman <mkulacha@gmail.com>
2019-03-04 00:11:56 -0500

    Preparation for push to live

--- website/ecosystem.config.js
+++ website/ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     // First application
     {
       name      : 'nazey',
-      script    : 'server/server.js',
+      script    : 'server.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
